fb4cb278ccc100bdfd4b6922053e01463918bd98
MrFuku <mmafighter_shun@icloud.com>
2020-05-31 15:18:20 +0900

    DB接続、CORSの設定

--- server/go.mod
+++ server/go.mod
@@ -4,5 +4,10 @@ go 1.14
 
 require (
 	github.com/99designs/gqlgen v0.11.3
+	github.com/go-chi/chi v3.3.2+incompatible
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/google/uuid v1.1.1
+	github.com/jinzhu/gorm v1.9.12
+	github.com/rs/cors v1.6.0
 	github.com/vektah/gqlparser/v2 v2.0.1
 )
--- server/server.go
+++ server/server.go
@@ -9,8 +9,15 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/MrFuku/kanban-go-nuxt-graphql/server/graph"
 	"github.com/MrFuku/kanban-go-nuxt-graphql/server/graph/generated"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/rs/cors"
+
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/go-chi/chi"
 )
 
+const dataSource = "localuser:localpass@tcp(db:3306)/localdb?charset=utf8&parseTime=True&loc=Local"
 const defaultPort = "8080"
 
 func main() {
@@ -19,11 +26,44 @@ func main() {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	db, err := gorm.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	if db == nil {
+		panic(err)
+	}
+	defer func() {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+	defer db.Close()
+	db.LogMode(true)
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	router := chi.NewRouter()
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	router.Use(cors.Handler)
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+	var mb int64 = 1 << 20
+	srv.AddTransport(transport.MultipartForm{
+		MaxMemory:     128 * mb,
+		MaxUploadSize: 100 * mb,
+	})
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
